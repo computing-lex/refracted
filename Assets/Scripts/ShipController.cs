607f228503c52d833b4f1ffd2817790970b1602f
unknown <bikminpikmin@gmail.com>
2024-03-30 12:43:52 -0400

    James Evil Branch

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class ShipController : MonoBehaviour
+public class ShipController : MonoBehaviour, IInteractable
 {
+
+    [SerializeField] private Transform standingPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,14 +14,12 @@ public class ShipController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 
     public void StartPiloting()
     {
         GameManager.instance.Player.SetState(GameManager.PlayerState.Piloting);
+        Debug.Log("Piloting now :3");
     }
 
     public void StopPiloting()
@@ -27,5 +28,20 @@ public class ShipController : MonoBehaviour
 
     }
 
+    public void Interact()
+    {
+        if (GameManager.instance.Player.GetState() == GameManager.PlayerState.Walking)
+        {
+            StartPiloting();
+        }
+        else
+        {
+            StopPiloting();
+        }
+    }
 
+    public Vector3 GetStandingPos()
+    {
+        return standingPos.position;
+    }
 }
--- Assets/Scripts/ShipCore.cs
+++ Assets/Scripts/ShipCore.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ShipCore : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private Vector3 velocity;
     void Start()
     {
         
@@ -13,6 +15,18 @@ public class ShipCore : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //DO NOT ROTATE THE SHIP WHEN PLAYER IS NOT PILOTING BTW
+        Vector3.MoveTowards(velocity, Vector2.zero, Time.deltaTime* 0.01f);
+        if (GameManager.instance.Player.GetState() == GameManager.PlayerState.Piloting)
+        {
+            velocity+= transform.TransformDirection(new Vector3(GameManager.instance.Player.GetPlayerInput().x,0,GameManager.instance.Player.GetPlayerInput().y));
+        }
+        //Vector3 movementThisFrame = new Vector3(0, 0, 1);
+        //Vector3 rotationThisFrame = new Vector3(0, 5, 0);
+        //transform.position += movementThisFrame*Time.deltaTime;
+        //transform.Rotate(rotationThisFrame * Time.deltaTime, Space.Self);
+       // GameManager.instance.Player.MoveWithShip(movementThisFrame*Time.deltaTime,rotationThisFrame * Time.deltaTime);
+
+
     }
 }
