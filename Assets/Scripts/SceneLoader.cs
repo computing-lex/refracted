5b9142fce1eecbaa1a83f3812bc710d85d285432
Kryptiax <kryptiax@gmail.com>
2024-03-30 22:40:42 -0400

    UI COMPLETE (MINUS THE ASSETS)
    feel free to fuck it around, for some reason the res values replicate and the button stays highlighted after being pressed but otherwise theres no bugs

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,16 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using System;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class SceneLoader : MonoBehaviour
 {
-    // sorry in advance that this exists idk how else to use unity
+    // sorry in advance that this exists idk how else to use unity :3
     [SerializeField] GameObject pauseMenu = null;
-    [SerializeField] GameObject resumeButton = null;
-    [SerializeField] GameObject settingsButton = null;
-    [SerializeField] GameObject quitButton = null;
+    [SerializeField] GameObject settingsMenu = null;
 
+    //TMP_Dropdown if i ever need that again :3
     bool isPaused;
+    Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+    
+
+    // for ui resolutions :3
+    public void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        Dropdown.OptionDataList resOptions = new Dropdown.OptionDataList();
+
+        int currentResIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++) 
+        {
+            Dropdown.OptionData option = new Dropdown.OptionData(resolutions[i].width + "x" + resolutions[i].height);
+            resOptions.options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResIndex = i;
+            }
+        }
+
+        // turn into strings for this to function correctly (syntax is a bitch)
+
+        resolutionDropdown.AddOptions(resOptions.options);
+        resolutionDropdown.value = currentResIndex;
+
+        Debug.Log(resOptions);
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void setResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
     public void setisPaused(bool settingto)
     {
@@ -27,16 +70,28 @@ public class SceneLoader : MonoBehaviour
 
             // fuck you ryan dumbass
             pauseMenu.SetActive(isPaused);
-            resumeButton.SetActive(isPaused);
-            settingsButton.SetActive(isPaused);
-            quitButton.SetActive(isPaused);
+            settingsMenu.SetActive(false);
         }
     }
-    
+
+
+    public AudioMixer audioMixer;
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("volumeMaster", volume);
+        Debug.Log(volume);
+    }
+
+    public void fullscreenBoolean(Boolean isFullscreen)
+    {
+        Debug.Log("Going to Fullscreen...");
+        Screen.fullScreen = isFullscreen;
+    }
+
     public void quitGame()
     {
-        Application.Quit();
         Debug.Log("Quitting Application...");
+        Application.Quit();
     }
 
     // start game - func right
@@ -55,14 +110,5 @@ public class SceneLoader : MonoBehaviour
     public void resumeGame()
     {
         Debug.Log("Resuming Game...");
-    }
-    public void setVolume(float volume)
-    {
-        Debug.Log(volume);
-    }
-
-    public void fullscreenBoolean()
-    {
-        Debug.Log("Going to Fullscreen...");
     }
 }
