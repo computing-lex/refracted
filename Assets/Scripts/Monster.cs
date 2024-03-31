e9ecd5e1bffe0efd54beb75da7e3927410d78850
Nebula <75294188+b0rah@users.noreply.github.com>
2024-03-31 02:44:07 -0400

    yeah

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -18,6 +18,11 @@ public class Monster : MonoBehaviour
     private int currentWaypoint;
 
 
+    private float monsterTimestamp;
+    private float monsterTimer1;
+    private float monsterTimer2;
+
+
     public enum MonsterPhase
     {
         Unaware,
@@ -39,6 +44,40 @@ public class Monster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
+        if(phase == MonsterPhase.Aware)
+        {
+            monsterTimer1 += Time.deltaTime;
+            if(monsterTimer1> monsterTimestamp)
+            {
+                phase = MonsterPhase.Approaching;
+                //play approaching roar
+                
+            }
+        }
+
+        //hi so
+        //i'm thinking there are a few ways for it to get you
+        //if you ping it and then sit still, you die
+        //you need to get away from the monster's range
+        //it waits a few seconds before roaring and approaching
+        //there's a 10% chance your ping won't affect it which just confuses you further
+        //want a lot of random chance involved here
+
+        //once you're a certain distance away we should probably implement a light switch.
+        //you need to get away then cut power. 
+        //once cut power, it'll scan manually
+        //if power is on you die
+        //avoid the scan - it scans once or twice
+        //and then it leaves for a while
+
+        //let me know if there's anything else you want to add/change for this.
+        //i really don't know how this ping system is gonna work so i don't know what else to do here
+        //i'll set up needed methods ig?
+
+
+
         if (phase == MonsterPhase.StartScan)
         {
             transform.position = waypoints[0].transform.position;
@@ -69,4 +108,20 @@ public class Monster : MonoBehaviour
     {
         phase = state;
     }
+
+    public void Pinged()
+    {
+        if (Random.Range(0, 10) != 1)
+        {
+            phase = MonsterPhase.Aware;
+            monsterTimestamp = Random.Range(10, 20);
+
+        }
+    }
+
+    public void Kill()
+    {
+
+    }
 }
+
